54b299e27e0ad19dbc9805c04db15370785b284a
ayushmaan-sh <ayushmaanshukla274@gmail.com>
2026-09-03 23:07:42 +0530

    Promisified functions of read and write file callbacks

--- promise.js
+++ promise.js
@@ -19,101 +19,48 @@
 
 // Once fulfilled or rejected, the Promise is **settled** and cannot change state. 
 
+const { rejects } = require("assert")
 const fs = require("fs")
 
 // promisified version of fs.readFile
-function fsReadFilePromisified(fileName, encoding) {
-    return new Promise(function (resolve, reject) {
-        fs.readFile(fileName, encoding, function (err, data) {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
-
-
-fsReadFilePromisified("file.txt", "utf-8")
-    .then(function (data) {
-        console.log(data)
-    })
-    .catch(function (err) {
-        console.log(err);
-    })
-
-
-// Promisified version of setTimeout
-
-
-function setTimeoutPromisified(time) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            resolve()
-        }, time)
-    })
-}
-
-setTimeoutPromisified(3000)
-    .then(function () {
-        console.log("3 seconds has been passed.")
-    })
-
-
-// const writeData = () =>{
-//     fs.writeFile("file.txt", "Hello JS!", "utf-8", (err)=>{
-//         if(err){
-//             console.log("Error while writing file:", err)
-//             return;
-//         } else {
-//             console.log("File written successfully!")
-//         }
-//     })
-// }
-
-// writeData()
-
-function writeFilePromisified(fileName, content, encoding){
+const readFilePromisified = (fileName, encoding) => {
     return new Promise(function(resolve, reject){
-        fs.writeFile(fileName, content, encoding, function(err){
+        fs.readFile(fileName, encoding, function(err, content){
             if(err){
-                reject(err)
+                reject()
+                console.log("Error while reading data.")
+                return
             } else {
-                resolve()
+                resolve(content)
             }
         })
     })
 }
 
-writeFilePromisified("file.txt", "Hello Ayushmaan!", "utf-8")
-    .then(console.log("File Written Successfully!"))
-    .catch(function(err){
-        console.log("Error While writing file:",err)
-    })
-
-
-// Assignment - Using fsReadFilePromisified and writeFilePromisified, make an new function called readAndWriteFilePromisified which reads the file first then convert the data into uppercase.
-
-const readAndWriteFilePromisified = (fileName, encoding) => {
-    return new Promise(function(resolve, reject){
-        fsReadFilePromisified(fileName, encoding)
-            .then(function(contents){
-                contents = contents.toUpperCase()
-                writeFilePromisified(fileName, contents, encoding)
-                resolve()
-            })
-    })
-}
-
-readAndWriteFilePromisified("file.txt", "utf-8")
-    .then(function(){
-        console.log("Data converted to uppercase!")
+readFilePromisified("file.txt", "utf-8")
+    .then(function(content){
+        console.log(content)
     })
     .catch(function(err){
         console.log(err)
     })
 
-// Tomorrow's revision topics : 
-// Write the promisified versions of - readFile, writeFile, setTimeout
-// do assignment  again
+const writeFilePromisified = (fileName, content, encoding) => {
+    return new Promise(function(resolve, reject){
+        fs.writeFile(fileName, content, encoding, function(err){
+            if(err){
+                reject()
+                console.log("Error while writing file!")
+                return;
+            } else {
+                resolve()
+            }
+        })
+    })
+}
+
+writeFilePromisified("file.txt", "Hello Ayushmaan", "utf-8")
+    .then(console.log("File written!"))
+    .catch(function(err){
+        console.log(err)
+    })
